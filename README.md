226b6de7c7ab6f40a67f0dcf278ddddb043ad87e
Tyler Etters <tyler@etters.co>
2021-08-20 13:31:34 -0500

    welcome to the lobby

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # bouncer
+
+As a member of Sidereal Lobby, I need to sync Octatracks with my bandmate.
+
+ 1. I expect to run a shell script that does all the syncing for me.
+ 2. I expect the shell script to use `rsync`.
+ 3. I expect my Octatrack to be plugged in and in USB mode.
+ 4. I expect each Octatrack to have a top level directory called `lobby`.
+ 5. I expect there to be a single directory in Dropbox called `lobby`.
+ 6. I expect the shell script and all Octatrack blessed files to reside in the same directory on Dropbox.
+ 7. I expect to be able to tell the shell script who I am so it knows my Octatrack's path.
+
+# The Architecture of Sidereral Lobby
+
+```
+| sidereal              -- dropbox top level shared can be named whatever you want
+  \  
+  | -- bouncer.sh       -- sync script
+  |
+  | -- readme.md        -- this file
+  |
+  | -- lobby/           -- the shared OT project directory
+  |     \ audio/        -- audio files synced via bouncer.sh
+  |     | projects/     -- project files synced via bouncer.sh
+  |
+  | --- ryan/           -- .gitignored
+  |     \ *.*           -- fodder
+  |
+  | --- tyler/          -- .gitignored
+  |     \ ableton/      -- main ableton files for stems and sketches
+  |     | compost/      -- whatever samples and garbage i make and want to share
+  |     | sketches/     -- typically bounced ableton projects for reference
+  |     | stems/        -- ready for octatrack or daw importing
+  |     
+  | --- images/         -- .gitignored
+  |      \ *.jpg
+  |      | *.png
+  |      | *.psd
+  |      | *.mp4
+  |      | (etc)
+  |
+  | --- releases/       -- .gitignored
+        \ 0000 0000     -- (spontaneous noise album)
+        | 0000 0001     -- (current)
+        | 0000 0010     -- (dub techno)
+```
---
+++ bouncer.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+function div() {
+  echo "================================================================================================"
+}
+
+div
+div
+div
+div
+
+cat <<"EOT"
+                                ____                     ____                                 
+            ..'''' | |``````.  |            |`````````, |                  .'.       |        
+         .''       | |       | |______      |'''|'''''  |______          .''```.     |        
+      ..'          | |       | |            |    `.     |              .'       `.   |        
+....''             | |......'  |___________ |      `.   |___________ .'           `. |_______ 
+                                                                                              
+            ______     ____       ____                                                        
+|         .~      ~.  |    ~.    |    ~.    ``..     ..''                                     
+|        |          | |____.'_   |____.'_       ``.''                                         
+|        |          | |       ~. |       ~.       |                                           
+|_______  `.______.'  |_______.' |_______.'       |                                           
+EOT
+
+function says() {
+  printf "\nBOUNCER: $1\n\n"
+}
+
+if [ $# -eq 0 ]; then
+    says "Welcome to the Lobby, please identify yourself with an \"r\" or \"t\" argument."
+    exit 1
+fi
+
+case $1 in
+  r)
+    authorized=1
+    user="Professor Laws"
+    octapath="/enter/your/path/here/ryan"
+    lobbypath="/enter/your/path/here/ryan"
+    ;;
+  t)
+    authorized=1
+    user="Professor Etters"
+    octapath="/Volumes/OCTATRACK/LOBBY"
+    lobbypath="/Users/mbp/Dropbox/People/sidereal/lobby"
+    ;;
+  *)
+    authorized=0
+    user="Guest"
+    octapath=""
+    ;;
+esac
+
+if [ $authorized = 1 ]; then
+  says "Welcome to the Lobby, $user."
+  div
+else
+  says "I regret to inform you the Lobby is not accepting new members at this time."
+  exit 0
+fi
+
+if [ -d  "$octapath" ]; then
+  says "I see your Octatrack is blessing up! Let's run df -h $octapath:"
+  df -h $octapath
+  div
+else
+  says "Your Octatrack is expected in room $octapath but sadly it cannot be accessed right now. Is it on and in USB mode?" 
+  exit 1
+fi
+
+if [ -d  "$lobbypath" ]; then
+  says "Let's also du -hs $lobbypath:"
+  du -hs $lobbypath
+  says "And diff $octapath $lobbypath:"
+  diff $octapath $lobbypath
+  div
+else
+  says "The shared lobby is expected at $lobbypath but sadly it cannot be accessed right now." 
+  exit 1
+fi
+
+says "Do you wish to proceed with rsync? 1 = yes, * = no."
+
+read a
+
+case $a in
+  1)
+    rsync -rtuv /$octapath/* /$lobbypath
+    rsync -rtuv /$lobbypath/* /$octapath
+    says "Enjoy your stay at Sidereal Lobby, $user."
+    exit 1
+    ;;
+  *)
+    says "Farewell."
+    exit 0
+    ;;
+esac
